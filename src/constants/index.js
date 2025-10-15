1a2ea731187779395375e9b334f1b2ed6f702a57
tJay11 <thamjoseph11@gmail.com>
2025-10-15 03:41:17 +0000

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,18 @@
+const navLinks = [
+	{ label: "Store" },
+	{ label: "Mac" },
+	{ label: "iPhone" },
+	{ label: "Watch" },
+	{ label: "Vision" },
+	{ label: "AirPods" },
+];
+
+export {
+	// features,
+	// featureSequence,
+	// footerLinks,
+	navLinks,
+	// noChangeParts,
+	// performanceImages,
+	// performanceImgPositions,
+};
