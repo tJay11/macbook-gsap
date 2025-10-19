479cb1b3c7dd9980b747e94a4a1c49dfed0504d2
tJay11 <thamjoseph11@gmail.com>
2025-10-19 16:43:56 +0000

    implemented showcase

--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -1,6 +1,30 @@
-import React from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useMediaQuery } from "react-responsive";
 
 const Showcase = () => {
+	const isTablet = useMediaQuery({ query: "(max-width: 1024px)" });
+
+	useGSAP(() => {
+		if (!isTablet) {
+			const timeline = gsap.timeline({
+				scrollTrigger: {
+					trigger: "#showcase",
+					start: "top top",
+					end: "bottom top",
+					scrub: true,
+					pin: true,
+				},
+			});
+
+			timeline
+				.to(".mask img", {
+					transform: "scale(1.1)",
+				})
+				.to(".content", { opacity: 1, y: 0, ease: "power1.in" });
+		}
+	}, [isTablet]);
+
 	return (
 		<section id="showcase">
 			<div className="media">
